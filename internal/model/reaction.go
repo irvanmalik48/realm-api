d7a58a38961fb3289833652425ee2715c45a0541
Irvan Malik <irvanma@gnuweeb.org>
2026-08-28 14:40:00 +0700

    feat(reactions): update allowed reactions to 3 positive and 3 negative reactions
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- internal/model/reaction.go
+++ internal/model/reaction.go
@@ -6,24 +6,23 @@ import (
 	"github.com/google/uuid"
 )
 
-// AllowedReactionTypes defines the set of valid reaction identifiers
+// AllowedReactionTypes defines the set of valid reaction identifiers (3 positive, 3 negative)
 var AllowedReactionTypes = map[string]bool{
-	"like":      true,
-	"love":      true,
-	"fire":      true,
-	"rocket":    true,
-	"mindblown": true,
-	"party":     true,
+	"like":    true,
+	"love":    true,
+	"fire":    true,
+	"dislike": true,
+	"frown":   true,
+	"skull":   true,
 }
 
 // PostReaction represents the database row for a post reaction
 type PostReaction struct {
-	ID               uuid.UUID  `json:"id"`
-	PostSlug         string     `json:"post_slug"`
-	ReactionType     string     `json:"reaction_type"`
-	UserID           *uuid.UUID `json:"user_id,omitempty"`
-	ClientIdentifier string     `json:"client_identifier"`
-	CreatedAt        time.Time  `json:"created_at"`
+	ID           uuid.UUID `json:"id"`
+	PostSlug     string    `json:"post_slug"`
+	ReactionType string    `json:"reaction_type"`
+	UserID       uuid.UUID `json:"user_id"`
+	CreatedAt    time.Time `json:"created_at"`
 }
 
 // PostReactionsResponse represents the summary of reactions for a blog post
@@ -31,6 +30,7 @@ type PostReactionsResponse struct {
 	Slug          string         `json:"slug"`
 	TotalCount    int            `json:"total_count"`
 	Reactions     map[string]int `json:"reactions"`
+	UserReaction  *string        `json:"user_reaction"`
 	UserReactions []string       `json:"user_reactions"`
 }
 
@@ -46,5 +46,6 @@ type ToggleReactionResponse struct {
 	Active        bool           `json:"active"`
 	TotalCount    int            `json:"total_count"`
 	Reactions     map[string]int `json:"reactions"`
+	UserReaction  *string        `json:"user_reaction"`
 	UserReactions []string       `json:"user_reactions"`
 }
